8d9bdbd75b637d4b697c195cc0e5856c672aa7bb
Nguyen Dinh Thuan <thuan.nguyendinh@hust.edu.vn>
2024-12-04 14:47:00 +0700

    demo room with thread add coroutines fix coroutines

--- app/src/main/java/com/example/demoroomdb/MainActivity.kt
+++ app/src/main/java/com/example/demoroomdb/MainActivity.kt
@@ -6,12 +6,13 @@ import com.example.demoroomdb.db.Color
 import com.example.demoroomdb.db.ColorDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
-    private val mainScope = CoroutineScope(Dispatchers.Main + Job())
+   // private val mainScope = CoroutineScope(Dispatchers.Main + Job())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         val colorDao = ColorDatabase.getInstance(application).colorDao()
         val newColor = Color(_id=0,hex = "#6200EE", name = "purple")
 
-        mainScope.launch {
+        GlobalScope.launch {
             withContext(Dispatchers.IO){
                 val colors = colorDao.getAll()
                 var x=1
